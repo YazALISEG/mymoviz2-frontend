5c0ec599cfa3f37a4f74985b6bc6e446300d5822
Your name <Your email>
2024-02-07 14:40:25 +0100

    test

--- components/Home.js
+++ components/Home.js
@@ -45,7 +45,7 @@ function Home() {
 //  ];
  
 useEffect(() => {
-    fetch('http://localhost:3000/movies')
+    fetch('https://my-moviz-backend-gold.vercel.app/')
 
     .then(response => response.json())
     .then(data => {
